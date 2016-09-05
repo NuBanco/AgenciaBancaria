553622f41ee6ad02aad1a66c5652ababddedbaca
Eliel <eliel.dourado>
2016-09-05 20:29:59 -0300

    Configurando tela login.

--- src/main/java/br/agencia/LoginUsuario.java
+++ src/main/java/br/agencia/LoginUsuario.java
@@ -42,18 +42,19 @@ public class LoginUsuario extends JFrame {
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				TelaBackground menuBancario = new TelaBackground(new HomeMenuBancario());
 				menuBancario.setSize(580, 470);
 				menuBancario.setLocationRelativeTo(null);
 				menuBancario.setVisible(true);
-				
+				dispose();
+
 				/*
-				TelaBackground menuCliente = new TelaBackground(new HomeMenuCliente());
-				menuCliente.setSize(580, 470);
-				menuCliente.setLocationRelativeTo(null);
-				menuCliente.setVisible(true);
-				*/	
+				 * TelaBackground menuCliente = new TelaBackground(new
+				 * HomeMenuCliente()); menuCliente.setSize(580, 470);
+				 * menuCliente.setLocationRelativeTo(null);
+				 * menuCliente.setVisible(true);
+				 */
 			}
 		});
 		btnLogin.setFont(new Font("Arial", Font.PLAIN, 15));
