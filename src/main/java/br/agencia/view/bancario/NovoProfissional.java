d2757429e2434f1ae93133ba11a2051e8535c912
Eduardo Henrique de Freitas <eduardo.freitas@datacoper.com.br>
2016-09-25 00:33:29 -0300

    voltar para lista quando confirmar

--- src/main/java/br/agencia/view/bancario/NovoProfissional.java
+++ src/main/java/br/agencia/view/bancario/NovoProfissional.java
@@ -118,6 +118,8 @@ public class NovoProfissional extends JPanel {
 					gravarUsuario(usuario);
 
 					limparTela();
+
+					TelaBackground.getPanelMenu().add(new ListaProfissional());
 				}
 
 			}
