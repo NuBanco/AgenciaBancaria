a51e44ea268ef461a4820c805fc0b09b8be48d49
Eliel <eliel.dourado>
2016-09-25 15:12:26 -0300

    Reestrurando tela Pagamento, para o novo padrão;

--- src/main/java/br/agencia/view/cliente/PagamentoCliente.java
+++ src/main/java/br/agencia/view/cliente/PagamentoCliente.java
@@ -4,17 +4,18 @@ import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.text.ParseException;
 
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.text.MaskFormatter;
 import javax.swing.SwingConstants;
 
 import br.agencia.view.principal.TelaBackground;
@@ -22,33 +23,28 @@ import br.agencia.view.principal.TelaBackground;
 public class PagamentoCliente extends JPanel {
 
 	private static final long serialVersionUID = -3894477076695872825L;
-	private JTextField txtCodigoBarras;
-	private JFormattedTextField txtValor;
+	private JTextField tfCodigoDeBarra;
+	private JFormattedTextField tfValor;
 
 	public PagamentoCliente() {
 
 		TelaBackground.getPanelMenu().add(new JPanel(), BorderLayout.CENTER);
 
-		JLabel lblCdigoDeBarras = new JLabel("C\u00F3digo de Barras");
-		lblCdigoDeBarras.setFont(new Font("Arial", Font.PLAIN, 14));
+		JLabel lbCdigoDeBarras = new JLabel("C\u00F3digo de Barras:");
+		lbCdigoDeBarras.setFont(new Font("Arial", Font.BOLD, 18));
 
-		txtCodigoBarras = new JTextField();
-		txtCodigoBarras.setFont(new Font("Arial", Font.PLAIN, 14));
-		txtCodigoBarras.setColumns(10);
+		tfCodigoDeBarra = new JTextField();
+		tfCodigoDeBarra.setFont(new Font("Arial", Font.PLAIN, 17));
+		tfCodigoDeBarra.setColumns(10);
 
-		txtValor = new JFormattedTextField();
-		txtValor.setText("0.00");
-		txtValor.setHorizontalAlignment(SwingConstants.RIGHT);
-		txtValor.setFont(new Font("Arial", Font.PLAIN, 14));
-		txtValor.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyTyped(KeyEvent arg0) {
-				String caractererNumerico = "0987654321.";
-				if (!caractererNumerico.contains(arg0.getKeyChar() + "")) {
-					arg0.consume();
-				}
-			}
-		});
+		tfValor = new JFormattedTextField();
+		tfValor.setHorizontalAlignment(SwingConstants.RIGHT);
+		tfValor.setFont(new Font("Arial", Font.PLAIN, 17));
+		try {
+			tfValor = new JFormattedTextField(new MaskFormatter("###.###.###,##"));
+		} catch (ParseException e1) {
+			JOptionPane.showMessageDialog(null, "Erro ao realizar o parser do valor!");
+		}
 
 		JButton btnConfirmar = new JButton("Confirmar");
 		btnConfirmar.addActionListener(new ActionListener() {
@@ -56,10 +52,10 @@ public class PagamentoCliente extends JPanel {
 
 			}
 		});
-		btnConfirmar.setFont(new Font("Arial", Font.BOLD, 14));
+		btnConfirmar.setFont(new Font("Arial", Font.PLAIN, 18));
 
-		JLabel lblValor = new JLabel("Valor a ser pago");
-		lblValor.setFont(new Font("Arial", Font.PLAIN, 14));
+		JLabel lbValor = new JLabel("Valor a ser pago:");
+		lbValor.setFont(new Font("Arial", Font.BOLD, 18));
 
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
@@ -69,34 +65,31 @@ public class PagamentoCliente extends JPanel {
 			}
 		});
 
-		btnVoltar.setFont(new Font("Arial", Font.BOLD, 14));
+		btnVoltar.setFont(new Font("Arial", Font.PLAIN, 18));
 		GroupLayout groupLayout = new GroupLayout(TelaBackground.getPanelMenu());
-		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
-				.createSequentialGroup().addGap(19)
-				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING).addComponent(lblValor).addGroup(
-						groupLayout
-								.createSequentialGroup()
-								.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-										.addComponent(txtValor, Alignment.LEADING)
-										.addComponent(txtCodigoBarras, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 150,
-												Short.MAX_VALUE)
-										.addComponent(lblCdigoDeBarras, Alignment.LEADING))
-								.addPreferredGap(ComponentPlacement.UNRELATED).addComponent(btnConfirmar)
-								.addPreferredGap(ComponentPlacement.RELATED).addComponent(btnVoltar)))
-				.addContainerGap(79, Short.MAX_VALUE)));
-		groupLayout
-				.setVerticalGroup(
-						groupLayout.createParallelGroup(Alignment.LEADING)
-								.addGroup(groupLayout.createSequentialGroup().addGap(23).addComponent(lblCdigoDeBarras)
-										.addPreferredGap(ComponentPlacement.RELATED)
-										.addComponent(txtCodigoBarras, GroupLayout.PREFERRED_SIZE,
-												GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addGap(18).addComponent(lblValor).addGap(2)
-										.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-												.addComponent(txtValor, GroupLayout.PREFERRED_SIZE, 21,
-														GroupLayout.PREFERRED_SIZE)
-												.addComponent(btnConfirmar).addComponent(btnVoltar))
-										.addContainerGap(180, Short.MAX_VALUE)));
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(Alignment.TRAILING,
+				groupLayout.createSequentialGroup().addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createSequentialGroup().addContainerGap().addComponent(btnVoltar)
+								.addGap(18).addComponent(btnConfirmar))
+						.addGroup(groupLayout.createSequentialGroup().addGap(187)
+								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING).addComponent(lbValor)
+										.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+												.addComponent(tfValor, Alignment.LEADING)
+												.addComponent(lbCdigoDeBarras, Alignment.LEADING,
+														GroupLayout.PREFERRED_SIZE, 211, GroupLayout.PREFERRED_SIZE)
+												.addComponent(tfCodigoDeBarra, Alignment.LEADING,
+														GroupLayout.DEFAULT_SIZE, 305, Short.MAX_VALUE)))))
+						.addGap(208)));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addGap(116).addComponent(lbCdigoDeBarras)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(tfCodigoDeBarra, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+								GroupLayout.PREFERRED_SIZE)
+						.addGap(18).addComponent(lbValor).addGap(2)
+						.addComponent(tfValor, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addGap(47).addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnConfirmar).addComponent(btnVoltar))
+						.addContainerGap(113, Short.MAX_VALUE)));
 		TelaBackground.getPanelMenu().setLayout(groupLayout);
 	}
 
