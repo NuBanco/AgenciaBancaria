35baf3bd9ca195bfc8426495bde2e48161d2c1c9
Eduardo Henrique de Freitas <eduardo.freitas@datacoper.com.br>
2016-09-27 19:49:58 -0300

    alterações de habilitar os botões

--- src/main/java/br/agencia/view/cliente/HomeMenuCliente.java
+++ src/main/java/br/agencia/view/cliente/HomeMenuCliente.java
@@ -14,6 +14,7 @@ import javax.swing.JPanel;
 
 import br.agencia.model.entidadesPersistidas.Conta;
 import br.agencia.model.enums.SituacaoConta;
+import br.agencia.model.enums.TipoConta;
 import br.agencia.model.enums.TipoMovimento;
 import br.agencia.model.util.UsuarioLogado;
 import br.agencia.view.principal.TelaBackground;
@@ -22,12 +23,20 @@ public class HomeMenuCliente extends JPanel {
 
 	private static final long serialVersionUID = 5853409558338193456L;
 	public static Conta contaCliente;
+
+	private JButton btnOpPagamento;
+	private JButton btnOpTransferencia;
+	private JButton btnOpDeposito;
+	private JButton btnOpSaldo;
+	private JButton btnOpSaque;
+	private JButton btnOpFinalizar;
+
 	DecimalFormat decimalFormat = new DecimalFormat("###,###,###,##0.00");
 
 	public HomeMenuCliente() {
 		createPanel(TelaBackground.getPanelMenu());
 
-		JButton btnOpSaque = new JButton("1 - Saque");
+		btnOpSaque = new JButton("1 - Saque");
 		btnOpSaque.setBounds(83, 62, 180, 30);
 		btnOpSaque.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -37,7 +46,7 @@ public class HomeMenuCliente extends JPanel {
 		});
 		btnOpSaque.setFont(new Font("Arial", Font.BOLD, 22));
 
-		JButton btnOpSaldo = new JButton("2 - Saldo");
+		btnOpSaldo = new JButton("2 - Saldo");
 		btnOpSaldo.setBounds(293, 116, 180, 30);
 		btnOpSaldo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -47,7 +56,7 @@ public class HomeMenuCliente extends JPanel {
 		});
 		btnOpSaldo.setFont(new Font("Arial", Font.BOLD, 22));
 
-		JButton btnOpDeposito = new JButton("3 - Dep\u00F3sitos");
+		btnOpDeposito = new JButton("3 - Dep\u00F3sitos");
 		btnOpDeposito.setBounds(293, 176, 180, 30);
 		btnOpDeposito.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -57,7 +66,7 @@ public class HomeMenuCliente extends JPanel {
 		});
 		btnOpDeposito.setFont(new Font("Arial", Font.BOLD, 22));
 
-		JButton btnOpTransferencia = new JButton("4 - Transfer\u00EAncias");
+		btnOpTransferencia = new JButton("4 - Transfer\u00EAncias");
 		btnOpTransferencia.setBounds(83, 116, 180, 30);
 		btnOpTransferencia.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -67,7 +76,7 @@ public class HomeMenuCliente extends JPanel {
 		});
 		btnOpTransferencia.setFont(new Font("Arial", Font.BOLD, 22));
 
-		JButton btnOpPagamento = new JButton("5 - Pagamentos");
+		btnOpPagamento = new JButton("5 - Pagamentos");
 		btnOpPagamento.setBounds(293, 62, 180, 30);
 		btnOpPagamento.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -77,7 +86,7 @@ public class HomeMenuCliente extends JPanel {
 		});
 		btnOpPagamento.setFont(new Font("Arial", Font.BOLD, 22));
 
-		JButton btnOpFinalizar = new JButton("6 - Finalizar");
+		btnOpFinalizar = new JButton("6 - Finalizar");
 		btnOpFinalizar.setBounds(83, 176, 180, 30);
 		btnOpFinalizar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -119,7 +128,17 @@ public class HomeMenuCliente extends JPanel {
 										.addComponent(btnOpFinalizar, GroupLayout.PREFERRED_SIZE, 50,
 												GroupLayout.PREFERRED_SIZE))
 						.addGap(111)));
+		configurarHome();
 		TelaBackground.getPanelMenu().setLayout(groupLayout);
+	}
+
+	private void configurarHome() {
+		if (UsuarioLogado.getContaUsuarioLogado().getTipoConta().equals(TipoConta.POUPANCA)) {
+			this.btnOpPagamento.setEnabled(false);
+		} else if (UsuarioLogado.getContaUsuarioLogado().getTipoConta().equals(TipoConta.ELETRONICA)) {
+			this.btnOpDeposito.setEnabled(false);
+			this.btnOpSaque.setEnabled(false);
+		}
 	}
 
 	private void createPanel(final JPanel panel) {
