2f8a9bd0fac48381f75403f7dd5bf73e173522a4
Eduardo Henrique de Freitas <eduardo.freitas@datacoper.com.br>
2016-09-26 23:54:52 -0300

    update na conta para desativar

--- src/main/java/br/agencia/view/cliente/FinalizaConta.java
+++ src/main/java/br/agencia/view/cliente/FinalizaConta.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 
 import javax.swing.JOptionPane;
 
+import br.agencia.control.GenericDao;
 import br.agencia.model.entidadesPersistidas.Conta;
 import br.agencia.model.enums.SituacaoConta;
 import br.agencia.model.util.UsuarioLogado;
@@ -17,17 +18,17 @@ public class FinalizaConta {
 		contaCliente = UsuarioLogado.getContaUsuarioLogado();
 		if (contaCliente.getSaldo().compareTo(new BigDecimal("0.00")) > 0) {
 			JOptionPane.showMessageDialog(null,
-					"Você possui um crédito em conta no valor de R$: " + (contaCliente.getSaldo())
-							+ (" . \n Para finalizar essa conta é necessário ter saldo zerado."),
+					"Vocï¿½ possui um crï¿½dito em conta no valor de R$: " + (contaCliente.getSaldo())
+							+ (" . \n Para finalizar essa conta ï¿½ necessï¿½rio ter saldo zerado."),
 					"Alerta", JOptionPane.WARNING_MESSAGE);
 		} else if (contaCliente.getSaldo().compareTo(new BigDecimal("0.00")) < 0) {
 			JOptionPane.showMessageDialog(null,
-					"Você possui um saldo devedor em conta no valor de R$: " + (contaCliente.getSaldo())
-							+ (" . \n Para finalizar essa conta é necessário ter saldo zerado."),
+					"Vocï¿½ possui um saldo devedor em conta no valor de R$: " + (contaCliente.getSaldo())
+							+ (" . \n Para finalizar essa conta ï¿½ necessï¿½rio ter saldo zerado."),
 					"Alerta", JOptionPane.WARNING_MESSAGE);
 		} else {
 			UsuarioLogado.getContaUsuarioLogado().setSituacaoConta(SituacaoConta.INATIVA);
-			JOptionPane.showMessageDialog(null, UsuarioLogado.getContaUsuarioLogado());
+			GenericDao.getGenericDao().alterar(UsuarioLogado.getContaUsuarioLogado());
 			JOptionPane.showMessageDialog(null,
 					"Conta " + contaCliente.getNumero().toString() + " finalizada com sucesso.");
 			System.exit(0);
